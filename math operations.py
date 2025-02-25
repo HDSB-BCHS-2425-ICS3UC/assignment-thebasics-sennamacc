766ddc5f9216071ea589c2c5af72a39a1ce9b93c
Senna <150617119+sennamacc@users.noreply.github.com>
2025-02-25 14:07:04 -0500

    Defined variables, wrote print statements, wrote comments

--- math operations.py
+++ math operations.py
@@ -1,13 +1,44 @@
-"""IMPORT LIBRARIES"""
-import math
-"""MATH"""
-sum_add = 2+23 #addition
+"""
+Perform Each math operations you have been shown (store the result in a variable) and print the result to the screen.
+
+Write a comment above each variable describing what the math operation does
+
+Difficulty Mark 2
+
+Author: Senna Chan Carusone
+Date Modified: February 24th, 2025
+Description: Describing different variable types.
+"""
+#SETUP
+import math #importing a necessary library for math calculations
+import time #importing this for fun
+
+#DEFINING VARIABLES
+sum = 2+23 #addition
 difference = 2-23 #subtraction
-product = 2*3*4 #multiplication
-quotient = int(25/8) #devision
-root = math.sqrt(9) #square root
-exponent = 2**4 #exponent
-rem_1 = 24%6 #modulus
-rem_2 = 24%5 #modulus again
-"""PRINT"""
-print(rem_1, rem_2)
+product = 2*2*3 #multiplication
+quotient = 23/2 #devision
+root = math.sqrt(9) #square root using math library
+exponent = 2**4 #exponent (2^4)
+remainder = 24%6 #modulus (finding the remainder from long devision)
+
+#PRINT STATEMENTS
+print("Math yay! (◐ ‿ ◑ )")
+time.sleep(2)
+print("Let's start with addition!\n2+23 = ",sum)
+time.sleep(2)
+print("And subtraction...\n2-23 = ",difference)
+time.sleep(2)
+print("Now let's do multiplication and devision ٩(•ิ˓̭ •ิ )ง\n2x2x3 = ",product,"\n23÷2 = ",quotient)
+time.sleep(2)
+print("Next up, square root.\n√9 = ",root)
+time.sleep(2)
+print("Exponentsssssssssss!!!\n2^4 = ",exponent)
+time.sleep(2)
+print("Finally, almost done!\nLastly, modulus (that means remainder).\n24%6 = ",remainder)
+time.sleep(2)
+print("Ok done.")
+time.sleep(3)
+print("Go away.")
+time.sleep(4)
+print("Um...")
