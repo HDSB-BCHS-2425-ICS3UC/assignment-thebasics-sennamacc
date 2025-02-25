e7f9f297ebbfa9f63a850d1f0d8ee7ac4edd0937
Senna <150617119+sennamacc@users.noreply.github.com>
2025-02-25 13:30:10 -0500

    Uploaded files that I accidentally committed to the wrong repo

---
+++ math operations.py
@@ -0,0 +1,13 @@
+"""IMPORT LIBRARIES"""
+import math
+"""MATH"""
+sum_add = 2+23 #addition
+difference = 2-23 #subtraction
+product = 2*3*4 #multiplication
+quotient = int(25/8) #devision
+root = math.sqrt(9) #square root
+exponent = 2**4 #exponent
+rem_1 = 24%6 #modulus
+rem_2 = 24%5 #modulus again
+"""PRINT"""
+print(rem_1, rem_2)
