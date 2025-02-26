049d4ea0e7bcba39ff4eb3b46270619fa6fcb57b
Senna <150617119+sennamacc@users.noreply.github.com>
2025-02-26 13:33:49 -0500

    Cleaned up code and added comments

--- discriminant.py
+++ discriminant.py
@@ -1,14 +1,22 @@
 """
 Author: Senna Chan Carusone
-Date Modified: February 25th, 2025
+Date Modified: February 26th, 2025
 Description: A program to calculate the discriminant, which is b^2 – 4ac (from the quadratic formula)
 """
-print("I will help you calculate discriminant. The discriminant is used in finding roots of a quadratic.")
+#Explaining to user via inital print statements
+print("I will help you calculate discriminant. The discriminant is used in finding roots (aka zeros) of a quadratic.")
 print("If it produces a negative number, there are no roots. If it produces zero, there is one root. And if it produces a positive number, there are two roots.")
+print("To calculate the discriminant, we need a quadratic in standard form, which is ax^2 + bx + c")
+print("You can now input values for a, b, and c, and I'll give you the answer by subbing your values into the discriminant.")
+
+#Getting user input
 a = float(input("Please input the value for a:\n"))
 b = float(input("Please input the value for b:\n"))
 c = float(input("Please input the value for c:\n"))
-
 print("Thank you!")
+
+#Calculating
 discriminant = b**2 - 4*a*c
+
+#Printing solution
 print("Discriminant =",discriminant)
