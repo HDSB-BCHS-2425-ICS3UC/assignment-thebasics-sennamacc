0aba5125a0f2ffb2242ced34235d64f3953dab1e
Senna <150617119+sennamacc@users.noreply.github.com>
2025-02-26 13:34:31 -0500

    Made final adjustments

--- math operations.py
+++ math operations.py
@@ -1,13 +1,7 @@
 """
-Perform Each math operations you have been shown (store the result in a variable) and print the result to the screen.
-
-Write a comment above each variable describing what the math operation does
-
-Difficulty Mark 2
-
 Author: Senna Chan Carusone
-Date Modified: February 24th, 2025
-Description: Describing different variable types.
+Date Modified: February 25th, 2025
+Description: Learning how to do different math operations in python.
 """
 #SETUP
 import math #importing a necessary library for math calculations
@@ -24,21 +18,22 @@ remainder = 24%6 #modulus (finding the remainder from long devision)
 
 #PRINT STATEMENTS
 print("Math yay! (◐ ‿ ◑ )")
-time.sleep(2)
-print("Let's start with addition!\n2+23 = ",sum)
-time.sleep(2)
-print("And subtraction...\n2-23 = ",difference)
-time.sleep(2)
-print("Now let's do multiplication and devision ٩(•ิ˓̭ •ิ )ง\n2x2x3 = ",product,"\n23÷2 = ",quotient)
-time.sleep(2)
-print("Next up, square root.\n√9 = ",root)
-time.sleep(2)
-print("Exponentsssssssssss!!!\n2^4 = ",exponent)
-time.sleep(2)
-print("Finally, almost done!\nLastly, modulus (that means remainder).\n24%6 = ",remainder)
-time.sleep(2)
+time.sleep(1)
+print("Let's start with addition!\nFor example, 2+23 = ",sum)
+time.sleep(1)
+print("And subtraction...\nFor exaxmple, 2-23 = ",difference)
+time.sleep(1)
+print("Now let's do multiplication and devision ٩(•ิ˓̭ •ิ )ง\nAn example of multiplication is, 2x2x3 = ",product,"\nAn example of devision is, 23÷2 = ",quotient)
+time.sleep(1)
+print("Next up, square root.\nFor example, √9 = ",root," That's becauase 3x3 equals 9.")
+time.sleep(1)
+print("Exponentsssssssssss!!!\nFor example, 2^4 = ",exponent)
+time.sleep(1)
+print("Finally, we're almost done!\nLastly, modulus (that means remainder).\nFor example, 24%6 = ",remainder)
+time.sleep(1)
 print("Ok done.")
 time.sleep(3)
 print("Go away.")
 time.sleep(4)
 print("Um...")
+time.sleep(5)
